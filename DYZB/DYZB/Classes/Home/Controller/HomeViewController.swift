3929a3dd0d53f663c376a0352ea66e64e2501184
manlili <644166434@qq.com>
2020-07-05 22:25:14 +0800

    在右边添加icon

--- DYZB/DYZB/Classes/Home/Controller/HomeViewController.swift
+++ DYZB/DYZB/Classes/Home/Controller/HomeViewController.swift
@@ -32,5 +32,26 @@ extension HomeViewController {
         btn.setImage(UIImage(named: "logo"), for: .normal)
         btn.sizeToFit()
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: btn)
+        
+        //设置首页右边的icon
+        let historyBtn = UIButton()
+        historyBtn.setImage(UIImage(named: "image_my_history"), for: .normal)
+        historyBtn.setImage(UIImage(named: "Image_my_history_click"), for: .highlighted)
+        historyBtn.frame = CGRect(x:0, y:0 , width: 40, height: 40)
+        let historyItem = UIBarButtonItem(customView: historyBtn)
+        
+        let serchBtn = UIButton()
+        serchBtn.setImage(UIImage(named: "btn_search"), for: .normal)
+        serchBtn.setImage(UIImage(named: "btn_search_clicked"), for: .highlighted)
+        serchBtn.frame = CGRect(x:0, y:0 , width: 40, height: 40)
+        let serchItem = UIBarButtonItem(customView: serchBtn)
+        
+        let qrcodeBtn = UIButton()
+        qrcodeBtn.setImage(UIImage(named: "Image_scan"), for: .normal)
+        qrcodeBtn.setImage(UIImage(named: "Image_scan_click"), for: .highlighted)
+        qrcodeBtn.frame = CGRect(x:0, y:0 , width: 40, height: 40)
+        let qrcodeitem = UIBarButtonItem(customView: qrcodeBtn)
+        
+        navigationItem.rightBarButtonItems = [historyItem, serchItem, qrcodeitem]
     }
 }
