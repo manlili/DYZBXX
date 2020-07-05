40d3d9c38ea32f6d349ca12e56802778bacca1f9
manlili <644166434@qq.com>
2020-07-05 23:04:57 +0800

    彻底优化完成右边icon的构造方法，主要是添加默认参数值

--- DYZB/DYZB/Classes/Tools/Extension/UIBarButtonItem-extension.swift
+++ DYZB/DYZB/Classes/Tools/Extension/UIBarButtonItem-extension.swift
@@ -22,11 +22,23 @@ extension UIBarButtonItem {
     */
     
     //第二种方法使用便利构造函数，必须满足2个条件:1、必须使用convenience开头，2、必须明确调用一个设计的构造函数，并且使用self
-    convenience init(imageName: String, highImageName: String) {
+    convenience init(imageName: String, highImageName: String = "", width: Int = 40, height: Int = 40) {
+        //定义btn
         let btn = UIButton()
+        
+        //设置btn的图片
         btn.setImage(UIImage(named: imageName), for: .normal)
-        btn.setImage(UIImage(named: highImageName), for: .highlighted)
-        btn.frame = CGRect(x:0, y:0 , width: 40, height: 40)
+        if highImageName != "" {
+            btn.setImage(UIImage(named: highImageName), for: .highlighted)
+        }
+        
+        //设置btn尺寸
+        if (width != 40 && height != 40) {
+            btn.sizeToFit()
+        }else {
+           btn.frame = CGRect(x:0, y:0 , width: width, height: height)
+        }
+        
         //注意最后必须使用self.init
         self.init(customView: btn)
     }
